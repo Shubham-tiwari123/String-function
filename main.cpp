d67b77dc6ec13ffd3a7e99fca101105b253cf805
Shubham-tiwari123 <st540362@gmail.com>
2018-03-18 00:14:49 +0530

    main file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "StringFunction.h"
+using namespace std;
 int main(){
     StringFunction s;
+    cout<<"Enter the string with space";
     s.acceptData();
-    s.displayData();
+    /*function to remove the space*/
     s.removeSpace();
     s.displayData();
+    cout<<"\nEnter the string with repetition";
+    s.acceptData();
+    /*function to remove the duplicate values*/
     s.removeDuplicate();
 }
-
