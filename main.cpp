885e93ca967c03aa0ddc0c39633393670fd63100
Shubham-tiwari123 <st540362@gmail.com>
2018-03-17 23:19:57 +0530

    main file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "StringFunction.h"
+int main(){
+    StringFunction s;
+    s.acceptData();
+    s.displayData();
+    s.removeSpace();
+    s.displayData();
+    s.removeDuplicate();
+}
+
