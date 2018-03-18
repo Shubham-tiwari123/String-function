9d777dbd04bee271b2e253bbe8e7ac3e09140465
Shubham-tiwari123 <st540362@gmail.com>
2018-03-18 20:35:00 +0530

    class file

--- StringFunction.h
+++ StringFunction.h
@@ -5,13 +5,22 @@ class StringFunction{
 private:
     char a[50];
     int i;
+    int count;
+    char data;
+    int num;
+    int flag[10];
+    int flag1 =0;
+    unsigned currentMax = 0;
+    unsigned maax = 0;
 public:
     void acceptData();
     void displayData();
     void removeSpace();
     void removeDuplicate();
+    void mostOccurance();
     StringFunction(){
         a[0] = 'a';
     }
 };
+
 #endif // STRINGFUNCTION_H_INCLUDED
